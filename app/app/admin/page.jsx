a4d545aeeb451b6d510f064836253f321c4d522c
lyes <lyesindel4@gmail.com>
2025-05-21 15:53:12 +0100

    added error message to login

--- app/app/admin/page.jsx
+++ app/app/admin/page.jsx
@@ -5,15 +5,20 @@ import { useActionState } from 'react'
 import { login } from '@/lib/actions'
 
 export default function Auth() {
-
+    // The state will contain the error message returned from the action
     const [state, action, pending] = useActionState(login, undefined)
 
     return (
         <div className={styles.wrapper} >
             <form className={styles.form} action={action} >
                 <h1>Connexion Admin</h1>
+                {/* Display error message if it exists */}
+                {state && <p className={styles.error}>{state}</p>}
                 <label htmlFor="password"></label>
                 <input id='password' type="password" name='password' placeholder="Mot de passe" />
+                
+                
+                
                 <button type='submit' >{pending ? 'Traitement' : 'Se connecter'}</button>
             </form>
         </div>
